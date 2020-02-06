89eccc59546657a94b40c9d9d2ecaf1b86b89a5b
Dima Lis <readspore@gmail.com>
2020-02-06 02:31:36 +0200

    tmp save

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -5,6 +5,10 @@ namespace KSAdmin.Models
 {
     public class ApplicationContext : IdentityDbContext<User>
     {
+        public DbSet<FileModel> Files { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
@@ -12,7 +16,33 @@ namespace KSAdmin.Models
 
             Database.EnsureCreated();
         }
-        public DbSet<FileModel> Files { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+            });
+
+            modelBuilder.Entity<Post>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Category>()
+                .HasKey(p => p.Id);
+
+            //modelBuilder.Entity<PostCategory>()
+            //    .HasKey(t => new { t.PostId, t.CategoryId });
+
+            //modelBuilder.Entity<PostCategory>()
+            //    .HasOne(pc => pc.Post)
+            //    .WithMany(s => s.PostCategorys)
+            //    .HasForeignKey(pc => pc.PostId);
+
+            //modelBuilder.Entity<PostCategory>()
+            //    .HasOne(pc => pc.Category)
+            //    .WithMany(c => c.PostCategorys)
+            //    .HasForeignKey(pc => pc.CategoryId);
+        }
     }
 }
---
+++ Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KSAdmin.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Slug { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int PostParent { get; set; }
+        public string Creation { get; set; }
+        public string Status { get; set; }
+        //public List<PostCategory> PostCategorys { get; set; }
+        //public Post()
+        //{
+        //    PostCategorys = new List<PostCategory>();
+        //}
+    }
+}
